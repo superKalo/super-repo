0133ac41bbfe98772bb9ff2d772038ceeba451d6
superKalo <superkalo@devlabs.bg>
2017-09-19 17:53:29 +0300

    Add: a comment

--- src/index.js
+++ src/index.js
@@ -88,6 +88,14 @@ class Repository {
     }
 };
 
+
+/**
+ * Uses Node, AMD or browser globals to create a module. This example creates
+ * a global even when AMD is used. This is useful if you have some scripts
+ * that are loaded by an AMD loader, but they still want access to globals.
+ *
+ * {@link https://github.com/umdjs/umd/blob/master/templates/returnExportsGlobal.js}
+ */
 (function (root, factory) {
     if (typeof define === 'function' && define.amd) {
         // AMD. Register as an anonymous module.
