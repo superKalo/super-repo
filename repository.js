f5449a5fdc52f464b4b746e74ad0ba4d2f626096
superKalo <superkalo@devlabs.bg>
2017-09-19 16:38:53 +0300

    Add: UMD pattern for exporting the module

--- repository.js
+++ repository.js
@@ -87,3 +87,22 @@ class Repository {
         this.syncInterval = null;
     }
 };
+
+
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define(['dependency'], factory);
+    } else if (typeof module === 'object' && module.exports) {
+        // Node. Does not work with strict CommonJS, but
+        // only CommonJS-like environments that support module.exports,
+        // like Node.
+        module.exports = factory(require('dependency'));
+    } else {
+        // Browser globals (root is window)
+        root.returnExports = factory(root.dependency);
+    }
+}(this, function (dependency) {
+    // Use dependency in some fashion.
+    return Repository;
+}));
