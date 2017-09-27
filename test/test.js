62d9d94dbf34500cfae81a3d9eafc7a3af4b97da
superKalo <superkalo@devlabs.bg>
2017-09-27 13:09:46 +0300

    Add: bootstrap promise-based tests; Add: test to get the main data

--- test/test.js
+++ test/test.js
@@ -1,15 +1,32 @@
 const chai = require('chai');
 const expect = chai.expect;
 
+const kindOfRegularResponse = require('./fake-api/kind-of-regular-response.json');
+
 const SuperRepo = require('../lib/index.js');
 const defaultConfiguration = {
+    storage: 'LOCAL_VARIABLE',
     name: 'test',
     outOfDateAfter: 3 * 1000, // 3 seconds
-    request: () => ({})
+    request: () => new Promise(resolve => resolve(kindOfRegularResponse))
 };
 
 describe('Library import', function() {
     it('Should make SuperRepo instance available.', function() {
         expect(new SuperRepo(defaultConfiguration)).to.be.an.instanceof(SuperRepo);
+    });
+});
+
+describe('Data Management', () => {
+    var repository;
+
+    before(() => {
+        repository = new SuperRepo(defaultConfiguration);
+    });
+
+    it('Should resolve to the server-side data', done => {
+        repository.getData().then( result => {
+            expect(result).to.equal(kindOfRegularResponse);
+        }).then(done, done);
     });
 });
