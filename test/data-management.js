73b54ac454dfa634bdfca7ebeda039f214644c8e
superKalo <superkalo@devlabs.bg>
2017-10-14 16:51:43 +0300

    Add: split data status related tests

--- test/data-management.js
+++ test/data-management.js
@@ -141,48 +141,6 @@ describe('Data Management', () => {
         });
     });
 
-    it('Should have up to date data.', done => {
-        repository.getData().then( () => {
-            repository.getDataUpToDateStatus().then(_res => {
-                expect(_res.isDataUpToDate).to.equal(true);
-                expect(_res.localData).to.equal(kindOfRegularResponse);
-            }).then(done, done);
-        });
-    });
-
-    it('Should initially have outdated data.', done => {
-        repository.getDataUpToDateStatus().then(_res => {
-            expect(_res.isDataUpToDate).to.equal(false);
-            expect(_res.localData).to.equal(null);
-        }).then(done, done);
-    });
-
-    it('Should have outdated data, when data gets invalidated.', done => {
-        repository.getData().then( () => {
-
-            repository.invalidateData().then( () => {
-                repository.getDataUpToDateStatus().then(_res => {
-                    expect(_res.isDataUpToDate).to.equal(false);
-                    expect(_res.localData).to.equal(kindOfRegularResponse);
-                }).then(done, done);
-            });
-
-        });
-    });
-
-    it('Should have outdated data, when data gets cleared.', done => {
-        repository.getData().then( () => {
-
-            repository.clearData().then( () => {
-                repository.getDataUpToDateStatus().then(_res => {
-                    expect(_res.isDataUpToDate).to.equal(false);
-                    expect(_res.localData).to.equal(null);
-                }).then(done, done);
-            });
-
-        });
-    });
-
     it('Should consider the cached data as always up to date and issue only 1 (network) request', done => {
         let networkRequestsCount = 0;
 
@@ -205,74 +163,6 @@ describe('Data Management', () => {
                     }).then(done, done);
                 });
             });
-        });
-    });
-
-    it('Should consider the cached data as always up to date', done => {
-        const repo = new SuperRepo({
-            storage: 'LOCAL_VARIABLE',
-            name: 'test',
-            outOfDateAfter: 0,
-            request: () => new Promise(resolve => resolve('whatever'))
-        });
-
-        repo.getData().then( () => {
-            clock.tick(31 * 24 * 60 * 1000); // 1 month
-
-            repo.getDataUpToDateStatus().then( _res => {
-                expect(_res.isDataUpToDate).to.equal(true);
-            }).then(done, done);
-        });
-    });
-
-    it('Should consider the cached data as always up to date, unless is invalidated', done => {
-        const repo = new SuperRepo({
-            storage: 'LOCAL_VARIABLE',
-            name: 'test',
-            outOfDateAfter: 0,
-            request: () => new Promise(resolve => resolve('whatever'))
-        });
-
-        repo.getData().then( () => {
-
-            repo.invalidateData().then(() => {
-                repo.getDataUpToDateStatus().then( _res => {
-                    expect(_res.isDataUpToDate).to.equal(false);
-                }).then(done, done);
-            });
-
-        });
-    });
-
-    it('Should consider the cached data as always up to date, unless is missing', done => {
-        const repo = new SuperRepo({
-            storage: 'LOCAL_VARIABLE',
-            name: 'test',
-            outOfDateAfter: 0,
-            request: () => new Promise(resolve => resolve('whatever'))
-        });
-
-        repo.getDataUpToDateStatus().then( _res => {
-            expect(_res.isDataUpToDate).to.equal(false);
-        }).then(done, done);
-    });
-
-    it('Should consider the cached data as always up to date, unless is cleared', done => {
-        const repo = new SuperRepo({
-            storage: 'LOCAL_VARIABLE',
-            name: 'test',
-            outOfDateAfter: 0,
-            request: () => new Promise(resolve => resolve('whatever'))
-        });
-
-        repo.getData().then( () => {
-
-            repo.clearData().then(() => {
-                repo.getDataUpToDateStatus().then( _res => {
-                    expect(_res.isDataUpToDate).to.equal(false);
-                }).then(done, done);
-            });
-
         });
     });
 });
